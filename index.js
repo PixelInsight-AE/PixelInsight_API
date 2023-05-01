2255c583474f16fccde3ed85ee3334c29d5e927f
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-01 11:08:49 -0600

    have proper response from server
    Co-authored-by:BlocksOnChainCode <BlocksOnChainCode@users.noreply.github.com>

--- index.js
+++ index.js
@@ -15,34 +15,8 @@ app.use((req, res, next) => {
 
 app.post("/api", async (request, response) => {
   console.log(request.body);
-  //send response to client
-  response.status(200).send("Hello from the server!");
+  response.status(200).send({ message: "success" });
 });
-
-/* app.post("/posts", async (req, res) => {
-  const { prompt } = req.body;
-  console.log(prompt);
-  try {
-    const response = await openai
-      .createImage({
-        prompt: prompt, // ? prompt is the text you want to generate an image from
-        n: 9, // ? n is the number of images you want to generate, sizes available are 256x256, 512x512, 1024x1024
-        size: "256x256", // ? size is the size of the image you want to generate
-      })
-      .catch((error) => {
-        console.log(`OPENAI ERR: ${error}`);
-      });
-
-    console.log(response.data);
-
-    const image_url = response.data;
-    console.log(image_url);
-
-    res.status(200).send(image_url);
-  } catch (error) {
-    console.log(`ERR: ${error}`);
-  }
-}); */
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
