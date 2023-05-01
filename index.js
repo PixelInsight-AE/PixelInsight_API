725c0a712c883e2c2f196864b4ce574e00fb24ec
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-01 14:52:11 -0600

    update
    Co-authored-by:BlocksOnChainCode <BlocksOnChainCode@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Pixel API Server");
+  res.send("Welcome to Pixel APi");
 });
 
 app.post("/api/OMDB", async (request, response) => {
