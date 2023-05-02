beac9b8237c4ccda87259542d873e8b56d5964ac
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-02 09:16:28 -0600

    update

---
+++ endpoints/OPENAI.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { Configuration, OpenAIApi } from "openai";
+
+const router = Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.post("/", async (req, res) => {
+  const { prompt } = req.body;
+  console.log(prompt);
+  try {
+    const response = await openai
+      .createImage({
+        prompt: prompt, // ? prompt is the text you want to generate an image from
+        n: 9, // ? n is the number of images you want to generate, sizes available are 256x256, 512x512, 1024x1024
+        size: "256x256", // ? size is the size of the image you want to generate
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+
+    console.log(response.data);
+
+    const image_url = response.data;
+    console.log(image_url);
+
+    res.status(200).send(image_url);
+  } catch (error) {
+    console.log(`ERR: ${error}`);
+  }
+});
+
+export default router;
