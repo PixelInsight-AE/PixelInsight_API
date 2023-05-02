7e80efd9e7ff08d1566956883e3c0828a4548eb6
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-02 08:17:16 -0600

    added endpoints folder and have omdb abstracted out, will add comments and work on open ai endpoint next

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-
+import OMDBRouter from "./endpoints/OMDB.js";
 dotenv.config();
 
 const app = express();
@@ -13,33 +13,59 @@ app.use((req, res, next) => {
   next();
 });
 
+//! setting up backend to have a viewable page
 app.get("/", (req, res) => {
-  res.send("Welcome to my Pixel API!");
+  res.send("Welcome to Pixel APi");
 });
 
-app.post("/api/OMDB", async (request, response) => {
-  console.log(request.body);
-  // response.status(200).send({ message: "success" });
-  const { movieSearch } = request.body;
-  console.log(movieSearch);
-  try {
-    const movie = await fetch(
-      `https://www.omdbapi.com/?s=${movieSearch}&apikey=${
-        process.env.OMDB_API_KEY || 73740781
-      }`
-    )
-      .then((res) => res.json())
-      .then((data) => data.Search);
+app.use("/api/OMDB", OMDBRouter);
 
-    console.log(movie);
-    response.status(200).send(movie);
+app.post("/api/OPENAI", async (req, res) => {
+  const { prompt } = req.body;
+  console.log(prompt);
+  try {
+    const response = await openai
+      .createImage({
+        prompt: prompt, // ? prompt is the text you want to generate an image from
+        n: 9, // ? n is the number of images you want to generate, sizes available are 256x256, 512x512, 1024x1024
+        size: "256x256", // ? size is the size of the image you want to generate
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+
+    console.log(response.data);
+
+    const image_url = response.data;
+    console.log(image_url);
+
+    res.status(200).send(image_url);
   } catch (error) {
-    console.log(error);
-    response.status(500).send({ message: "error" });
+    console.log(`ERR: ${error}`);
   }
 });
 
-const port = process.env.PORT || 3000;
+// app.post("/api/OMDB", async (request, response) => {
+//   console.log(request.body);
+//   // response.status(200).send({ message: "success" });
+//   const { movieSearch } = request.body;
+//   console.log(movieSearch);
+//   try {
+//     const movie = await fetch(
+//       `https://www.omdbapi.com/?s=${movieSearch}&apikey=${process.env.OMDB_API_KEY}`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => data.Search);
+
+//     console.log(movie);
+//     response.status(200).send(movie);
+//   } catch (error) {
+//     console.log(error);
+//     response.status(500).send({ message: "error" });
+//   }
+// });
+
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
