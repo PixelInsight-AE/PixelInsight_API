72ed2727bf015eb9abc8e859ec2c5d1dd2679795
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-05-02 11:02:10 -0600

    all cleaned up

--- index.js
+++ index.js
@@ -29,31 +29,6 @@ app.use("/api/OMDB", OMDBRouter);
 //? calling the OPENAIRouter from endpoints/OPENAI.js
 app.use("/api/OPENAI", OPENAIRouter);
 
-// app.post("/api/OPENAI", async (req, res) => {
-//   const { prompt } = req.body;
-//   console.log(prompt);
-//   try {
-//     const response = await openai
-//       .createImage({
-//         prompt: prompt, // ? prompt is the text you want to generate an image from
-//         n: 9, // ? n is the number of images you want to generate, sizes available are 256x256, 512x512, 1024x1024
-//         size: "256x256", // ? size is the size of the image you want to generate
-//       })
-//       .catch((error) => {
-//         console.log(`OPENAI ERR: ${error}`);
-//       });
-
-//     console.log(response.data);
-
-//     const image_url = response.data;
-//     console.log(image_url);
-
-//     res.status(200).send(image_url);
-//   } catch (error) {
-//     console.log(`ERR: ${error}`);
-//   }
-// });
-
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
